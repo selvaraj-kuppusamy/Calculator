733d9e7ebe310de67d51cb2af9b2cfb012555872
selvaraj-kuppusamy <velmanikvps@gmail.com>
2020-11-10 16:17:15 +0530

    addition

---
+++ Addition/Addition.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "ADDITION"
+echo "Enter the Number of Numbers to add"
+read NUMBERS
+i=0
+SUM=0
+echo "Enter the Numbers"
+while [ $i -lt $NUMBERS ]
+do
+	read NUMBER
+	SUM=`expr $SUM + $NUMBER`
+	i=`expr $i + 1`
+done
+
+echo "THE SUM OF NUMBERS"
+echo "$SUM"
+
