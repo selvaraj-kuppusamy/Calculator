fcbbb41634a8413fda755b4d2bafb6be56de99b3
selvaraj-kuppusamy <velmanikvps@gmail.com>
2020-11-10 16:55:13 +0530

    Modulus

---
+++ Modulus/Modulus.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "MODULUS"
+echo "Enter the Number01"
+read NUMBER01
+echo "Enter the Number02"
+read NUMBER02
+
+MOD=`expr $NUMBER01 % $NUMBER02`
+
+echo "The Modulus Of Two Numbers "
+echo "$MOD"
+
