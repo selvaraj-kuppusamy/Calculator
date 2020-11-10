52b4d1a39efe6840fb438bd09d7d11baec02123b
selvaraj-kuppusamy <velmanikvps@gmail.com>
2020-11-10 16:44:12 +0530

    multiplication

---
+++ Multiplication/Multilication.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "MULTIPLICATION"
+echo "Enter the Number01"
+read NUMBER01
+echo "Enter the Number02"
+read NUMBER02
+
+MUL=`expr $NUMBER01 \* $NUMBER02`
+
+echo "The Multilication  Of Two Numbers "
+echo "$MUL"
+
