5459a9285c5d6853455d9f3d79b02fb8aeb4401e
selvaraj-kuppusamy <velmanikvps@gmail.com>
2020-11-10 16:49:15 +0530

    division

---
+++ Division/Division.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "DIVISION"
+echo "Enter the Number01"
+read NUMBER01
+echo "Enter the Number02"
+read NUMBER02
+
+DIV=`expr $NUMBER01 / $NUMBER02`
+
+echo "The Division Of Two Numbers "
+echo "$DIV"
+
