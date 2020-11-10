70dc84ae59b62e5ab7d8fd6664e0f1483c51754f
selvaraj-kuppusamy <velmanikvps@gmail.com>
2020-11-10 16:35:37 +0530

    subtraction

---
+++ Subtraction/Subtraction.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "SUBTRACTION"
+
+echo "Enter the Number01"
+read NUMBER01
+echo "Enter the Number02"
+read NUMBER02
+
+SUB=`expr $NUMBER01 - $NUMBER02`
+
+echo "The Subtraction Of Two Numbers "
+echo "$SUB"
+
+
